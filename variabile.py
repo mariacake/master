9d0ab31c15b3ddeebc9d7d5d10479870b730df1a
maria <maria.zanescu@gmail.com>
2023-02-20 19:08:05 +0200

    general rules variables

---
+++ variabile.py
@@ -0,0 +1,14 @@
+varsta_copil_mic = 18
+varsta_copil_mare = 17
+
+print(varsta_copil_mic + varsta_copil_mare)
+print('copil mic', varsta_copil_mic)
+print("varsta copil mic = ", varsta_copil_mic)
+
+nr_1 = varsta_copil_mic
+nr_2 = varsta_copil_mare
+nr_3 = 258
+nr_4 = nr_1**3
+
+print("rezultatul la toata treaba asta este ", nr_4 - nr_3)
+
