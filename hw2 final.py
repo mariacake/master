5df2109b3a4189fe55b41907525a856b5acedf05
maria <maria.zanescu@gmail.com>
2023-03-08 08:33:49 +0200

    hw2 varianta ok

---
+++ hw2 final.py
@@ -0,0 +1,12 @@
+tries = 3
+while tries > 0:
+    try:
+        age = int(input('age: '))
+        if age>=18:
+            print('u are grown up')
+        elif age<18:
+            print('u are a child')
+            break
+    except ValueError:
+        tries = tries - 1
+        print('not a number')
