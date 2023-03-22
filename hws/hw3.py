8fa9a53d4dd7da26eef2606a35e486d95dc7b42e
maria <maria.zanescu@gmail.com>
2023-03-22 14:49:48 +0200

    tema 3 update

--- hws/hw3.py
+++ hws/hw3.py
@@ -1,58 +1,63 @@
-# #1
-#
+# 1
 
-# def hw_f1(*args, **kwargs):
-#     sum_num = 0
-#     for integer in args:
-#         if type(integer) is int:
-#             sum_num += integer
-#     return sum_num
-#
-# hw = hw_f1(3, 4, 345, 'm', [1, 2, 3], param1 = 'abc')
-# print(hw)
-#
 
-# #2
-#
+def hw_f1(*args, **kwargs):
+    sum_num = 0
+    for integer in args:
+        if type(integer) is int:
+            sum_num += integer
+    return sum_num
 
-# def hw_f2(n: int) ->tuple:
-#     if n == 0:
-#         return 0, 0, 0
-#
-#     sumatotal, even, odd = hw_f2(n-1)
-#
-#     sumatotal += n
-#
-#     if n % 2 == 0:
-#         even += n
-#
-#     else:
-#         odd += n
-#
-#     return sumatotal, even, odd
-#
-# sumtotal_g, even_g, odd_g = hw_f2(28)
-#
-# print(sumtotal_g)
-# print(even_g)
-# print(odd_g)
-#
-#
-# #3
-#
 
-# def hw_f3():
-#     user_input = input('val:')
-#     try:
-#         return int(user_input)
-#
-#     except ValueError:
-#         return 0
-#
-# hw_ff = hw_f3()
-# print(hw_ff)
+hw = hw_f1(1, 10, 100, 'm', [5, 50, 500], param_1='abc')
+print(hw)
 
-#4?
+
+# 2
+
+
+def hw_f2(n: int) -> tuple:
+
+    if n == 0:
+        return 0, 0, 0
+
+    sum_total, even_sum, odd_sum = hw_f2(n-1)
+    sum_total += n
+
+    if n % 2 == 0:
+        even_sum += n
+
+    else:
+        odd_sum += n
+
+    return sum_total, even_sum, odd_sum
+
+
+total_g, even_g, odd_g = hw_f2(28)
+
+print(total_g)
+print(even_g)
+print(odd_g)
+
+
+# 3
+
+
+def hw_f3():
+    user_input = input('val:')
+    try:
+        return int(user_input)
+
+    except ValueError:
+        return 0
+
+
+hw_v = hw_f3()
+print(hw_v)
+
+
+# 4?
+
 
 def hw_f4(*args, **kwargs):
     sum_num = 0
@@ -76,11 +81,11 @@ def hw_f4(*args, **kwargs):
                     sum_num += value
     for int_value in kwargs.values():
         if type(int_value) == int:
-            sum_num += int_number
+            sum_num += int_value
         elif type(int_value) == list:
             for value_in_list in int_value:
                 if type(value_in_list) == int:
-                   sum_num += value_in_list
+                    sum_num += value_in_list
         elif type(int_value) == str:
             try:
                 int_value = int(int_value)
@@ -90,13 +95,10 @@ def hw_f4(*args, **kwargs):
         elif type(int_value) == dict:
             for value in int_value.values():
                 if type(value) == int:
-                   sum_num += value
+                    sum_num += value
 
     return sum_num
 
 
-
-hw = hw_f4(1, 2, 3, '4', [1, 2, 3], param1 = [1, 2])
+hw = hw_f4(1, 2, 3, '4', [1, 2, 3], param1=[1, 2])
 print(hw)
-
-
